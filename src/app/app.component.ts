b167a078e3b0d7d974e000334e39cfbd466db8de
salwashaker <salwashaker90@gmail.com>
2018-07-06 20:11:57 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { Tab1Page } from '../pages/tab1/tab1';
+import { Tab2Page } from '../pages/tab2/tab2';
+import { Tab3Page } from '../pages/tab3/tab3';
+import { Tab4Page } from '../pages/tab4/tab4';
+import { Tab5Page } from '../pages/tab5/tab5';
+import { Tab6Page } from '../pages/tab6/tab6';
+import { Tab7Page } from '../pages/tab7/tab7';
+import { Tab8Page } from '../pages/tab8/tab8';
+import { Tab9Page } from '../pages/tab9/tab9';
+import { Tab10Page } from '../pages/tab10/tab10';
+import { Tab11Page } from '../pages/tab11/tab11';
 
 @Component({
   templateUrl: 'app.html'
@@ -21,8 +32,19 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'مرحبا بك', component: HomePage },
+      { title: 'طرق التغذية الصحيحة لمرضى السكري', component: Tab1Page },
+      { title: 'نصائح لمرضى السكري في رمضان', component: Tab2Page },
+      { title: 'فحوصات مرض السكري', component: Tab3Page },
+      { title: 'استخدام الاعشاب في علاج مرض السكري', component: Tab4Page },
+      { title: 'كيفية التعامل مع مرضى السكري', component: Tab5Page },
+      { title: 'معدل السكر التراكمي', component: Tab6Page },
+      { title: 'نصائح لمرضى السكري تتعلق بالصحة والرياضة', component: Tab7Page },
+      { title: 'اسباب المرض ', component: Tab8Page },
+      { title: 'طرق الوقاية من المرض', component: Tab9Page },
+      { title: 'سكر الحمل وطرق الوقاية منه', component: Tab10Page },
+      { title: 'طرق علاج جروح مرضى السكري', component: Tab11Page }
+      
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,36 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Tab1Page } from '../pages/tab1/tab1';
+import { Tab2Page } from '../pages/tab2/tab2';
+import { Tab3Page } from '../pages/tab3/tab3';
+import { Tab4Page } from '../pages/tab4/tab4';
+import { Tab5Page } from '../pages/tab5/tab5';
+import { Tab6Page } from '../pages/tab6/tab6';
+import { Tab7Page } from '../pages/tab7/tab7';
+import { Tab8Page } from '../pages/tab8/tab8';
+import { Tab9Page } from '../pages/tab9/tab9';
+import { Tab10Page } from '../pages/tab10/tab10';
+import { Tab11Page } from '../pages/tab11/tab11';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    Tab1Page,
+    Tab2Page,
+    Tab3Page,
+    Tab4Page,
+    Tab5Page,
+    Tab6Page,
+    Tab7Page,
+    Tab8Page,
+    Tab9Page,
+    Tab10Page,
+    Tab11Page
   ],
   imports: [
     BrowserModule,
@@ -23,7 +43,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    Tab1Page,
+    Tab2Page,
+    Tab3Page,
+    Tab4Page,
+    Tab5Page,
+    Tab6Page,
+    Tab7Page,
+    Tab8Page,
+    Tab9Page,
+    Tab10Page,
+    Tab11Page
   ],
   providers: [
     StatusBar,
